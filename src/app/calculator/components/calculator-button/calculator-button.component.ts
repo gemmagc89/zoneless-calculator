76b3cbd17c87ea9e740a5372fdb02a4d8ae9d060
Gemma <ggc.gemma@gmail.com>
2025-01-30 19:22:52 +0100

    fin seccion 4

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostBinding, input, OnInit, output, signal, viewChild } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -6,12 +6,19 @@ import { Component, HostBinding, input, OnInit } from '@angular/core';
   templateUrl: './calculator-button.component.html',
   styleUrl: './calculator-button.component.css',
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400',
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()',
     // attribute: 'hola',
     // 'data-size': 'XL',
   }
 })
 export class CalculatorButtonComponent {
+
+  public isPressed = signal(false);
+
+  public onClick = output<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
 
   public isCommand = input(false, {
     transform: ( value: boolean | string ) =>
@@ -22,8 +29,29 @@ export class CalculatorButtonComponent {
       typeof value === 'string' ? value === '' : value,
   });
 
-  @HostBinding('class.w-2/4') get commandStyle() {
+  /*@HostBinding('class.w-2/4') get commandStyle() {
     return this.isDoubleSize();
+  }*/
+
+  handleClick() {
+    if(!this.contentValue()?.nativeElement) {
+      return;
+    }
+    const value = this.contentValue()!.nativeElement.innerText;
+    this.onClick.emit(value.trim());
+  }
+
+  public keyboardPressedStyle(key: string) {
+    if(!this.contentValue()) return;
+
+    const value = this.contentValue()?.nativeElement.innerText;
+
+    if(value !== key) return;
+
+    this.isPressed.set(true);
+    setTimeout( ()=> {
+      this.isPressed.set(false);
+    }, 100);
   }
 
 }
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, viewChildren } from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
 
 @Component({
   selector: 'calculator',
   imports: [CalculatorButtonComponent],
   templateUrl: './calculator.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)'
+  }
   /*styles: `
     @reference "tailwindcss";
     .is-command {
@@ -13,5 +18,44 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   `*/
 })
 export class CalculatorComponent {
+
+  private calculatorService = inject(CalculatorService);
+
+  public resultText = computed( () => this.calculatorService.resultText() )
+  public subResultText = computed( () => this.calculatorService.subResultText() )
+  public lastOperator = computed( () => this.calculatorService.lastOperator() )
+
+  /*get resultText() {
+    return this.calculatorService.resultText();
+  }*/
+
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+
+
+  handleClick(key: string) {
+    this.calculatorService.constructNumber(key);
+  }
+
+  // @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent){
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      'x': '*',
+      '/': '÷',
+      '+': '+',
+      ',': '.',
+      Enter: '='
+    }
+
+    const key = event.key;
+    const keyValue = keyEquivalents[key] ?? key;
+
+    this.handleClick(keyValue);
+
+    this.calculatorButtons().forEach( button => {
+      button.keyboardPressedStyle(keyValue);
+    });
+  }
 
 }
